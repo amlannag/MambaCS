e98d0ce67f6cc1647bebdefe9c2d98b24a96b0f7
amlannag <amlannag12@gmail.com>
2026-06-29 12:48:10 +1000

    Update submit_oasis.sh

--- submit_oasis.sh
+++ submit_oasis.sh
@@ -31,6 +31,8 @@ echo "Start time : $(date)"
 echo "Working dir: $(pwd)"
 echo ""
 
+pip install fastmri
+
 unset SLURM_MEM_PER_GPU SLURM_MEM_PER_CPU SLURM_MEM_PER_NODE
 srun --cpu-bind=none python image_domain_testing_brain_mri.py \
     --train_dir /scratch/user/uqanag/OASIS/keras_png_slices_train \
