25ce07d1a2de5178a30d67f405cc20d9b127d3fe
amlannag <amlannag12@gmail.com>
2026-09-04 13:02:39 +1000

    Update submit_amd.sh

--- submit_amd.sh
+++ submit_amd.sh
@@ -18,7 +18,7 @@ set -eo pipefail
 export WANDB_API_KEY='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'
 
 source /sw/local/rocky8/noarch/rcc/software/miniforge/24.11.3-0/etc/profile.d/conda.sh
-conda activate mambacs-AMD
+conda activate mambacs-rocm714
 
 set -u
 hash -r
